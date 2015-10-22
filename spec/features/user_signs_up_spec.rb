79ff4e0266e7efc77728427166143f0f08a89da9
Nate <npauzenga@gmail.com>
2015-10-22 12:43:46 -0400

    feature spec for User Signs Up

---
+++ spec/features/user_signs_up_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+feature "User signs up" do
+  scenario "Successfully"do
+    visit root_path
+    within ".nav" do
+      click_link "Sign Up"
+    end
+    fill_in "Name", with: "Ted Danson"
+    fill_in "Password", with: "helloworld"
+    fill_in "Email", with: "tdanson@remembercheers.com"
+
+    within "form" do
+      click_button "Sign Up"
+    end
+    expect(current_path).to eq sign_in_path
+    expect(page).to
+    have_content("Thanks! Please check your email to complete sign up")
+    # expect confirmation email to send
+  end
+
+  xscenario "and confirms account via email link" do
+    # ???
+  end
+end
