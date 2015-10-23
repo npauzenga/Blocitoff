8c0d2aa4f4f80a2fd16047285630faaac5287aba
Nate <npauzenga@gmail.com>
2015-10-23 16:29:47 -0400

    moved email confirm specs to controller_specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,10 +1,21 @@
 require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  let(:unconfirmed_user) { build(:user) }
+  
+  describe "email confirmation" do
+    it "sends a confirmation email when user registers" do
+      allow(UserMailer)
+        .to receive(:registration_confirmation)
+        .with(unconfirmed_user)
+        .and_return(double(deliver_now: true))
+
+      expect(UserMailer).to receive(:registration_confirmation)
+      unconfirmed_user.save
+    end
+
+    xit "does not allow an unconfirmed user to sign in" do
+      #TODO
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,6 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  let(:unconfirmed_user) { build(:user) }
   let(:user_static_email) { create(:user, email: "test@test.com") }
   let(:user_invalid_email) { build(:user, email: "xxx") }
 
@@ -13,22 +12,6 @@ RSpec.describe User, type: :model do
     it "rejects a duplicate email address" do
       user_static_email
       expect(build(:user, email: "test@test.com")).to_not be_valid
-    end
-  end
-
-  describe "email confirmation" do
-    it "sends a confirmation email when user registers" do
-      allow(UserMailer)
-        .to receive(:registration_confirmation)
-        .with(unconfirmed_user)
-        .and_return(deliver_now: true)
-
-      expect(UserMailer).to receive(:registration_confirmation)
-      unconfirmed_user.save
-    end
-
-    xit "does not allow an unconfirmed user to sign in" do
-      #TODO
     end
   end
 end
