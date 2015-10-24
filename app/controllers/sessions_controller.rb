ae225e8ccf8367ddd818bfec1cd3b47fa9617fd8
Nate <npauzenga@gmail.com>
2015-10-23 20:36:58 -0400

    log in working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      redirect_to @user
+    else
+      flash.now[:error] = "There was a problem signing in"
+      render "new"
+    end
+  end
+
+  def destroy
   end
 end
