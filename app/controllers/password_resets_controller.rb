483600d50357f87b5d5e23dcb61707a74cf037b9
Nate <npauzenga@gmail.com>
2015-10-24 14:31:01 -0400

    completed password reset without tests

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,         only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -15,6 +18,34 @@ class PasswordResetsController < ApplicationController
     else
       flash.now[:error] = "Email address not found"
       render "new"
+    end
+  end
+
+  def update
+    if @user.update_attributes(user_params)
+      log_in @user 
+      flash[:success] = "Password has been reset"
+      redirect_to @user
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+    redirect_to root_url if @user == nil
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired"
+      redirect_to new_password_reset_url
     end
   end
 end
