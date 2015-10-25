43b8510c1499f13e32f37af00ed5e1b11b2301dc
Nate <npauzenga@gmail.com>
2015-10-25 16:45:54 -0400

    feature spec for user deletes todo

---
+++ spec/features/user_deletes_todo_spec.rb
@@ -0,0 +1,21 @@
+require "spec_helper"
+
+feature "User deletes todo" do
+  let(:user) { create(:user, email_confirmed: true) }
+
+  scenario "Successfully" do
+    visit root_path
+    within ".nav" do
+      click_link "Sign In"
+    end
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    within "form" do
+      click_button "Sign In"
+    end
+    fill_in "Description", with: "Meet up with the team"
+    click_button "Save"
+    click_link ""
+  end
+end
