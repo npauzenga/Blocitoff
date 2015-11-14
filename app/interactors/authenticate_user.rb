adce84626f069ba688b19763d13a73f5c3ae438e
Nate <npauzenga@gmail.com>
2015-11-14 16:25:05 -0500

    User authentication now verifies email_confirmed correctly

--- app/interactors/authenticate_user.rb
+++ app/interactors/authenticate_user.rb
@@ -6,12 +6,16 @@ class AuthenticateUser
     authenticate_user(context.user, context.password)
   end
 
+  private
+
   def authenticate_user(user, password)
-    return if user && password_vaild? && user.email_confirmed
+    return if user && password_valid?(password) && context.user.email_confirmed
     context.fail!(errors: "invalid Email or Password")
   end
 
-  def password_valid?
-    Encryptor.passwords_match?(password, user.password_salt, user.password_hash)
+  def password_valid?(password)
+    Encryptor.passwords_match?(password,
+                               context.user.password_salt,
+                               context.user.password_hash)
   end
 end
