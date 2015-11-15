8e78aee530a3dd6c6d799fa5be105045d732da39
Nate <npauzenga@gmail.com>
2015-11-15 16:36:12 -0500

    testing POST #create, not working

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,11 +3,45 @@ require "rails_helper"
 RSpec.describe UsersController, type: :controller do
   let(:user) { create(:unconfirmed_user) }
 
-  describe "email confirmation" do
-    it "sends a confirmation email when user registers" do
-      expect {
-        UserMailer.registration_confirmation(user).deliver_now
-      }.to change(ActionMailer::Base.deliveries, :size).by(1)
+  describe "POST #create" do
+    let(:interactor_input) do
+      {:user =>
+        { name: user.name,
+          email: user.email,
+          password: user.password,
+          password_confirmation: user.password_confirmation
+        }
+      }
     end
+
+    let(:interactor_context) do
+      Interactor::Context.new(errors: :val, user: user)
+    end
+
+    before(:example) do
+      allow(CreateUser).to receive(:call).with(interactor_input).
+        and_return(interactor_context)
+    end
+
+    context "when valid params are given" do
+      it "calls the CreateUser interactor" do
+        expect(CreateUser).to receive(:call)
+        post :create, interactor_input
+      end
+
+      it "returns http status 201" do
+
+      end
+
+      it "renders view X" do
+
+      end
+    end
+
+    context "when email is not provided" do
+
+    end
+
+
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,11 +1,13 @@
 FactoryGirl.define do
-  factory :user do
+  factory :confirmed_user, class: User do
     name "Abraham Lincoln"
     sequence(:email, 100) { |n| "user#{n}@example.com" }
     password "helloworld"
     password_confirmation "helloworld"
     email_confirmed true
     after(:create) { |user| user.confirm_token = nil }
+    after(:create) { |user| user.confirm_digest = nil }
+    after(:create) { |user| EncryptPassword.call(user: user) }
   end
 
   factory :unconfirmed_user, class: User do
@@ -14,5 +16,8 @@ FactoryGirl.define do
     password "helloworld"
     password_confirmation "helloworld"
     email_confirmed false
+    after(:create) { |user| user.confirm_token = SecureRandom.urlsafe_base64.to_s }
+    after(:create) { |user| user.confirm_digest = Encryptor.digest_token(user.confirm_token) }
+    after(:create) { |user| EncryptPassword.call(user: user) }
   end
 end
