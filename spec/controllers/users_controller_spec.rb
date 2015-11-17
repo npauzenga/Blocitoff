f83c6fdb793b552a96caeeaa9012cdc148e096dc
Nate <npauzenga@gmail.com>
2015-11-17 11:34:07 -0500

    "when valid params are given" works correctly with interactor_input

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe UsersController, type: :controller do
   let(:user) { create(:unconfirmed_user) }
 
   describe "POST #create" do
+    let(:params) { { user: interactor_input.fetch(:user_params) } }
+
     let(:interactor_input) do
-      {:user =>
-        { name: user.name,
+      { user_params: {
+          name: user.name,
           email: user.email,
           password: user.password,
           password_confirmation: user.password_confirmation
@@ -19,28 +21,44 @@ RSpec.describe UsersController, type: :controller do
     end
 
     before(:example) do
-      allow(CreateUser).to receive(:call).with(interactor_input).
-        and_return(interactor_context)
+      allow(CreateUser).to receive(:call).with(interactor_input)
+        .and_return(interactor_context)
     end
 
     context "when valid params are given" do
       it "calls the CreateUser interactor" do
         expect(CreateUser).to receive(:call)
-        post :create, interactor_input
+        post :create, params
       end
 
-      it "returns http status 201" do
-
-      end
-
-      it "renders view X" do
-
-      end
+    #   # 200 because we're redirecting to sign_in?
+    #   it "returns http status 200" do
+    #     expect(response).to have_http_status(200)
+    #     post :create, interactor_input
+    #   end
+    #
+    #   it "renders view sessions#new" do
+    #     expect(post :create, interactor_input).to redirect_to(sign_in_path)
+    #   end
     end
-
-    context "when email is not provided" do
-
-    end
+    #
+    # # it'll still call CreateUser.call but this has been tested?
+    # context "when email is not provided" do
+    #   it "redirects to sign_up" do
+    #     interactor_input[:user][:email] = nil
+    #     expect(post :create, interactor_input).to redirect_to(sign_up_path)
+    #   end
+    # end
+    #
+    # context "when password is not provided" do
+    #   it "redirects to sign_up" do
+    #   end
+    # end
+    #
+    # context "when password confirmation is not provided" do
+    #   it "redirects to sign_up" do
+    #   end
+    # end
 
 
   end
