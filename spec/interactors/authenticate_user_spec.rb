2a07de312a6cf04796b71a5c67eabc9fc4550067
Nate <npauzenga@gmail.com>
2015-11-24 12:00:05 -0500

    revise spec to use implicit subject rather than explicit

--- spec/interactors/authenticate_user_spec.rb
+++ spec/interactors/authenticate_user_spec.rb
@@ -6,47 +6,47 @@ RSpec.describe AuthenticateUser do
     let(:unconfirmed_user) { create(:unconfirmed_user) }
 
     context "when given valid credentials" do
-      subject(:context) do
+      subject do
         described_class.call(email: user.email, password: user.password)
       end
 
       it "succeeds" do
-        expect(context).to be_a_success
+        is_expected.to be_a_success
       end
 
       it "provides the user" do
-        expect(context.user).to eq(user)
+        expect(subject.user).to eq(user)
       end
     end
 
     context "when given invalid user" do
-      subject(:context) do
+      subject do
         described_class.call(email: "fake@bs.com", password: user.password)
       end
 
       it "fails" do
-        expect(context).to be_a_failure
+        is_expected.to be_a_failure
       end
     end
 
     context "when given invalid password" do
-      subject(:context) do
+      subject do
         described_class.call(email: user.email, password: "nope")
       end
 
       it "fails" do
-        expect(context).to be_a_failure
+        is_expected.to be_a_failure
       end
     end
 
     context "when email not confirmed" do
-      subject(:context) do
+      subject do
         described_class.call(email:    unconfirmed_user.email,
                              password: unconfirmed_user.password)
       end
 
       it "fails" do
-        expect(context).to be_a_failure
+        is_expected.to be_a_failure
       end
     end
   end
