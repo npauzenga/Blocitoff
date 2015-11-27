0038143a3457d783225dabd09cdde333e1d77e2b
Nate <npauzenga@gmail.com>
2015-11-26 22:59:52 -0500

    refactor create_confirmation_token and finish specs

--- app/interactors/create_confirmation_token.rb
+++ app/interactors/create_confirmation_token.rb
@@ -2,12 +2,17 @@ class CreateConfirmationToken
   include Interactor
 
   def call
+    context.fail!(error: "could not set token") unless context.user
     context.enc, context.raw = Encryptor.generate_token
-    context.user.confirm_token = context.raw
+    update_confirm_token
     update_confirm_digest
   end
 
   private
+
+  def update_confirm_token
+    context.user.confirm_token = context.raw
+  end
 
   def update_confirm_digest
     context.user.update_attribute(:confirm_digest, context.enc)
