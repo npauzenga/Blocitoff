d55699fdcc137f280b4e350d01e51352d3d4ab37
Nate <npauzenga@gmail.com>
2015-11-29 16:06:00 -0500

    incredibly satisfying refactor

--- spec/interactors/create_password_reset_token_spec.rb
+++ spec/interactors/create_password_reset_token_spec.rb
@@ -10,24 +10,15 @@ RSpec.describe CreatePasswordResetToken do
       end
 
       it "sets the user's reset token" do
-        original_token = user.reset_token
-        create_reset_token
-        new_token = user.reset_token
-        expect(original_token).not_to eq(new_token)
+        expect { create_reset_token }.to change { user.reset_token }
       end
 
       it "sets the user's reset_digest" do
-        original_digest = user.reset_digest
-        create_reset_token
-        new_digest = user.reset_digest
-        expect(original_digest).not_to eq(new_digest)
+        expect { create_reset_token }.to change { user.reset_digest }
       end
 
       it "sets the user's reset_sent_at" do
-        original_reset_sent_at = user.reset_sent_at
-        create_reset_token
-        new_reset_sent_at = user.reset_sent_at
-        expect(original_reset_sent_at).not_to eq(new_reset_sent_at)
+        expect { create_reset_token }.to change { user.reset_sent_at }
       end
     end
   end
