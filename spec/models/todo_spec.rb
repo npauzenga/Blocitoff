831316d185d4a4a59d3450372194bdc48208042e
Nate <npauzenga@gmail.com>
2015-11-30 19:26:05 -0500

    complete spec for Todo model

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,26 +1,28 @@
 require "rails_helper"
 
-RSpec.describe Todo, type: :model do
-  include TestFactories
-  describe "Validations" do
-    let(:todo) { valid_todo }
-
-    describe "description validation" do
-      it "only allows a Todo to be saved with a description" do
-        todo.description = nil
-        expect(todo.save).to eq false
-      end
+RSpec.describe Todo do
+  context "attributes" do
+    %w(description user_id).each do |attribute|
+      it { is_expected.to have_attribute attribute }
     end
   end
 
-  describe "methods" do
-    let(:todo) { valid_todo }
+  context "validations" do
+    it { is_expected.to validate_presence_of :description }
+  end
+
+  context "methods" do
+    let(:todo) { create(:todo) }
 
     describe "#days_left" do
-      it "displays the days left until deletion" do
+      it "returns the days left until deletion" do
         todo.update_attribute(:created_at, (DateTime.now.utc - 1))
         expect(todo.days_left).to eq 6
       end
     end
+  end
+
+  context "relationships" do
+    it { is_expected.to belong_to :user }
   end
 end
