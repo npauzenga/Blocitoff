01f7f97ae279fdf00d34ed7867be49ada2876e86
Nate <npauzenga@gmail.com>
2015-12-06 11:23:55 -0500

    add input and output validation + tests to interactor

--- spec/interactors/create_password_reset_token_spec.rb
+++ spec/interactors/create_password_reset_token_spec.rb
@@ -21,5 +21,29 @@ RSpec.describe CreatePasswordResetToken do
         expect { create_reset_token }.to change { user.reset_sent_at }
       end
     end
+
+    context "when user is invalid" do
+      subject(:create_reset_token) do
+        described_class.call(user: nil)
+      end
+
+      it "fails" do
+        expect(create_reset_token).to be_a_failure
+      end
+    end
+
+    context "when reset token isn't properly assigned" do
+      subject(:create_reset_token) do
+        described_class.call(user: user)
+      end
+
+      before(:example) do
+        allow(Encryptor).to receive(:generate_token).and_return(nil)
+      end
+
+      it "fails" do
+        expect(create_reset_token).to be_a_failure
+      end
+    end
   end
 end
